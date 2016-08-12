3cc5660d083779f25386ef4592c8186402482c52
Emily Chiu <miniapple8888@gmail.com>
2016-08-12 18:04:35 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       log_in @user
       remember @user
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = "Invalid password/email combination"
       render 'new'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:emily)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +13,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
+    log_in_as(@user)
+    get root_path
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', '#', text: "Account"
   end
 end
