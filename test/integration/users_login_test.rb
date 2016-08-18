09e37a61b8c7d129ef11cbab18c7013fee6c1fff
Emily Chiu <miniapple8888@gmail.com>
2016-08-17 22:22:41 -0400

    Add account activations

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,7 +34,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login without remembering" do
     log_in_as(@user, remember_me: '0')
-    assert_not_nil cookies['remember_token']
+    assert_nil cookies['remember_token']
   end
 
 end
