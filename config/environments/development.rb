cd03caf058e432206a5a3d9d6b724c70acb356b0
Emily Chiu <miniapple8888@gmail.com>
2016-08-18 22:37:38 -0400

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
       :port => 1025
   }
   host = "localhost:3000"
-  config.action_mailer.default_url_options = { host: host, protocol: "https" }
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
